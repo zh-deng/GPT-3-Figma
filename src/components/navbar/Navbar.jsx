3a896021f925d3bda1b957318b6849f5fbe78eee
zh-deng <heromemo@web.de>
2023-02-13 18:30:07 +0100

    small bugs fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,9 +7,9 @@ const Menu = () => (
     <>
     <p><a href="#home">Home</a></p>
     <p><a href="#wgpt3">What is GPT?</a></p>
-    <p><a href="possibilities">Open AI</a></p>
-    <p><a href="features">Case Studies</a></p>
-    <p><a href="blog">Library</a></p>
+    <p><a href="#possibilities">Open AI</a></p>
+    <p><a href="#features">Case Studies</a></p>
+    <p><a href="#blog">Library</a></p>
     </>
 )
 
